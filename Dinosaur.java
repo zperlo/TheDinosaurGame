8b754f7ba572f24a0e8c09b3746f5b7560d8b10d
Dylan <dylan@Dylans-MacBook-Pro.local>
2018-09-10 14:47:11 -0400

    Dinosaur and Runner classes revised to be nicer, Runner still needs most dino cards added.

--- Dinosaur.java
+++ Dinosaur.java
@@ -11,11 +11,22 @@ public class Dinosaur {
    public int intelligence;
    public int defenses;
    public int weapons;
-   public int sense;
+   public int senses;
    public int ror; // Rate of Reproduction
    public int ata; // Ability to adapt
    
-   public Dinosaur(String n, boolean b, String h, int sp, int si, int i, int d, int w, int se, int r, int a) {
-   
+   public Dinosaur(String name, boolean isHerbivore, String habitat, int speed, int size, int intelligence,
+                   int defenses, int weapons, int senses, int ror, int ata) {
+      this.name = name;
+      this.isHerbivore = isHerbivore;
+      this.habitat = habitat;
+      this.speed = speed;
+      this.size = size;
+      this.intelligence = intelligence;
+      this.defenses = defenses;
+      this.weapons = weapons;
+      this.senses = senses;
+      this.ror = ror;
+      this.ata = ata;
    }
 }
