ba98bf2df30158dbcb3c7f7a0275626f1fa45be6
zperlo <zperlo@commschool.org>
2018-09-11 11:09:03 -0400

    added more to natural disaster card, started natural disaster deck

--- NaturalDisasterCard.java
+++ NaturalDisasterCard.java
@@ -1,21 +1,49 @@
 public class NaturalDisasterCard {
-   public String text;
-   public boolean habitatSafe;
-   public int stat; // 0-7 are normal stats, 8 is no stat required
-   public int[] safeStatValues;
-   public int foodTokensLost;
+   private String text;
+   private int habitatSafe;
+   private String stat;
+   private int[] safeStatValues = new int[0];
+   private int foodTokensLost = 0;
+   // 0-7 are normal stats, 8 is no stat required
+   private String[] stats = {"Speed", "something1", "something2", "something3", "something4", "something5", "something6", "none"};
    
-   public NaturalDisasterCard(String text, boolean habitatSafe, int stat){
+   public NaturalDisasterCard(String text, int habitatSafe, int statNum){
       this.text = text;
       this.habitatSafe = habitatSafe;
-      this.stat = stat;
+      this.stat = this.stats[statNum];
    }
    
-   public NaturalDisasterCard(String text, boolean habitatSafe, int stat, int[] safeStatValues, int foodTokensLost){
+   public NaturalDisasterCard(String text, int habitatSafe, int statNum, int[] safeStatValues, int foodTokensLost){
       this.text = text;
       this.habitatSafe = habitatSafe;
-      this.stat = stat;
+      this.stat = this.stats[statNum];
       this.safeStatValues = safeStatValues;
       this.foodTokensLost = foodTokensLost;
+   }
+   
+   public String getText(){
+      return this.text;
+   }
+   
+   public int getHabitatSafe(){
+      return this.habitatSafe;
+   }
+   
+   public String getStat(){
+      return this.stat;
+   }
+   
+   public int[] getSafeStatValues(){
+      return this.safeStatValues;
+   }
+   
+   public int getFoodLost(){
+      return this.foodTokensLost;
+   }
+   
+   public static void main(String[] potato){
+      int[] safeStats = {0, 1};
+      NaturalDisasterCard flood = new NaturalDisasterCard("Flood, watch out", 0, 0, safeStats, 3);
+      System.out.println("If you have no " + flood.getStat() + " then you lose " + flood.getFoodLost() + " food tokens");
    }
 }
---
+++ NaturalDisasterDeck.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class NaturalDisasterDeck {
+   private final int deckSize = 20;
+   private NaturalDisasterCard[] deck = new NaturalDisasterCard[deckSize];
+   
+   public NaturalDisasterCard draw(){
+      int rand = (int)(Math.random() * (this.deckSize);
+      return this.deck[rand];
+   }
+}
