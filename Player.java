fdb631442fa10e9dc47291584c4fb9ff49f5f4b6
zperlo <zperlo@commschool.org>
2018-10-01 14:44:32 -0400

    added player class

---
+++ Player.java
@@ -0,0 +1,48 @@
+public class Player {
+    private Dinosaur myDino;
+    private int foodTokens;
+    private int location = 0;
+    private boolean evolveCardSenInt = false;
+    private boolean evolveCardSpdSiz = false;
+
+    public Player(Dinosaur myDino, int foodTokens){
+        this.myDino = myDino;
+        this.foodTokens=foodTokens;
+    }
+
+    public Dinosaur getDino(){
+        return myDino;
+    }
+
+    public int getFoodTokens(){
+        return foodTokens;
+    }
+
+    public int getLocation(){
+        return location;
+    }
+
+    public boolean isEvolveCardSenInt(){
+        return evolveCardSenInt;
+    }
+
+    public boolean isEvolveCardSpdSiz(){
+        return evolveCardSpdSiz;
+    }
+
+    public void move(int numSpaces){
+        location = location + numSpaces;
+    }
+
+    public void changeFood(int numChange){
+        foodTokens = foodTokens + numChange;
+    }
+
+    public void setEvolveCardSenInt(boolean set){
+        evolveCardSenInt = set;
+    }
+
+    public void setEvolveCardSpdSiz(boolean set){
+        evolveCardSpdSiz = set;
+    }
+}
