ea774ea6b9d8c030277ae4f4268c891d3182f6d3
Dylan <dylan@Dylans-MacBook-Pro.local>
2018-10-08 13:03:33 -0400

    Space revised

--- Space.java
+++ Space.java
@@ -1,24 +1,15 @@
 public class Space {
 
     private String habitat;
-    private String currentType; // challenge, natural disaster, danger zone, attack, start, or finish
-    private String trueType; // whatever it was without something occupying it
+    private String type; // challenge, natural disaster, danger zone, start, finish, carnivore, herbivore
 
-    public Space(String habitat, String trueType) {
+    public Space(String habitat, String type) {
         this.habitat = habitat;
-        this.trueType = trueType;
+        this.type = type;
     }
 
-    public void occupy() {
-        currentType = "attack";
-    }
-
-    public void leave() {
-        currentType = trueType;
-    }
-
-    public String getCurrentType() {
-        return currentType;
+    public String getType() {
+        return type;
     }
 
     public String getHabitat() {
