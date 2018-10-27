0e4e39f9b904983e5437cc5798a56f9f31767c2f
Tyler Anderson <twa16@case.edu>
2018-10-27 11:40:29 -0400

    blah

--- TestGame.java
+++ TestGame.java
@@ -27,7 +27,47 @@ public class TestGame { //when TestGame file is run, it runs all test methods, n
     }
 
     @Test public void testNaturalDisaster(){
+        Player p1 = new Player(new Dinosaur("TestDino1", true, "Forest",1,1,
+                1, 1,1,1,1,1), 5);
+        //one player to pass all tests, one to fail all tests
+        Player p2 = new Player(new Dinosaur("TestDino2", true, "Desert",-1,-1,
+                -1, -1,-1,-1,-1,-1), 5);
+        NaturalDisasterDeck ndDeck1 = new NaturalDisasterDeck();
+        int[] arr1 = {1};
+        NaturalDisasterCard card1 = new NaturalDisasterCard("", "", "", false, "intelligence",
+                arr1, 2);
+        for(int i = 0; i < 20; i++)
+            ndDeck1.setDeck(i, card1);
 
+        NaturalDisasterDeck ndDeck2 = new NaturalDisasterDeck();
+        NaturalDisasterCard card2 = new NaturalDisasterCard("", "", "", true, "none");
+        for(int i = 0; i < 20; i++)
+            ndDeck2.setDeck(i, card2);
+        //make my own decks for different types for penalties
+        //deck1 for testing safe values and food token loss
+        //deck2 for habitat safe testing and turn loss
+        //space 0 is a forest habitat so p1 should be habitat safe, but p2 won't be.
+        //covers both safe conditions and both penalties with two players
+        Space[] board = Game_GUI.createBoard();
+
+        int food1 = p1.getFoodTokens();
+        int food2 = p2.getFoodTokens();
+        int turns1 = p1.getLostTurns();
+        //int turns2 = p2.getLostTurns();
+
+        Game_GUI.naturalDisaster(p1, ndDeck1, board);
+        assertEquals(food1, p1.getFoodTokens());
+        Game_GUI.naturalDisaster(p2, ndDeck1, board);
+        assertEquals(food2 - 2, p2.getFoodTokens());
+
+        System.out.println(board[p1.getLocation()].getHabitat());
+        System.out.println(p1.getDino().getHabitat());
+        System.out.println(board[p2.getLocation()].getHabitat());
+        System.out.println(p2.getDino().getHabitat());
+        Game_GUI.naturalDisaster(p1, ndDeck2, board);
+        assertEquals(turns1, p1.getLostTurns());
+        Game_GUI.naturalDisaster(p2, ndDeck2, board);
+        assertEquals(2, p2.getLostTurns());
     }
 
     @Test public void testDangerZone(){
